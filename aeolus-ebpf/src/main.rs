8b98944544643be7592c09c7b45191fa075b29ce
OmarAlraisi <o.r.raisi@gmail.com>
2024-03-08 23:03:43 +0400

    redirects UDP and TCP packets to a hard coded MAC address

--- aeolus-ebpf/src/main.rs
+++ aeolus-ebpf/src/main.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
 
-use core::mem;
-use network_types::{eth::{EthHdr, EtherType}, ip::{IpProto, Ipv4Hdr}, tcp::TcpHdr, udp::UdpHdr};
 use aya_bpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
+use core::mem;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+    tcp::TcpHdr, udp::UdpHdr,
+};
 
 #[xdp]
 pub fn aeolus(ctx: XdpContext) -> u32 {
@@ -26,20 +30,24 @@ fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
     }
 }
 
+fn ptr_at_mut<T>(ctx: &XdpContext, offset: usize) -> Result<*mut T, ()> {
+    Ok((ptr_at::<T>(ctx, offset)?) as *mut T)
+}
+
 fn try_aeolus(ctx: XdpContext) -> Result<u32, ()> {
-    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
+    let ethhdr: *mut EthHdr = ptr_at_mut(&ctx, 0)?;
     let mut offset = EthHdr::LEN;
 
     match unsafe { *ethhdr }.ether_type {
-        EtherType::Ipv4 => {},
-        _ => return Ok(xdp_action::XDP_PASS)
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
     };
 
     let iphdr: *const Ipv4Hdr = ptr_at(&ctx, offset)?;
     offset += Ipv4Hdr::LEN;
-
-    let src_addr = u32::from_be(unsafe { *iphdr }.src_addr);
-    let dst_addr = u32::from_be(unsafe { *iphdr }.dst_addr);
+    
+    let src_ip = u32::from_be(unsafe { *iphdr }.src_addr);
+    let dst_ip = u32::from_be(unsafe { *iphdr }.dst_addr);
 
     let (src_port, dst_port) = match unsafe { *iphdr }.proto {
         IpProto::Tcp => {
@@ -54,11 +62,25 @@ fn try_aeolus(ctx: XdpContext) -> Result<u32, ()> {
             let dst_port = u16::from_be(unsafe { *udphdr }.dest);
             (src_port, dst_port)
         }
-        _ => return Ok(xdp_action::XDP_PASS)
+        _ => return Ok(xdp_action::XDP_PASS),
     };
-    
-    info!(&ctx, "SRC: {:i}:{}, DST: {:i}:{}", src_addr, src_port, dst_addr, dst_port);
-    Ok(xdp_action::XDP_PASS)
+
+    // Check if port should be balanced
+    if dst_port != 3000 {
+        return Ok(xdp_action::XDP_PASS);
+    }
+
+    // TODO: calculate 4-tuple hash
+    // TODO: Get MAC address of the appropriate server
+
+    // Modify destination MAC address
+    unsafe {
+        (*ethhdr).dst_addr = [0x52, 0x54, 0x00, 0x94, 0xdf, 0x40];
+    }
+
+    let mac = unsafe { *ethhdr }.dst_addr;
+    info!(&ctx, "SRC: {:i}:{}, DST: {:i}:{} ---> redirecting to MAC: {:mac}", src_ip, src_port, dst_ip, dst_port, mac);
+    Ok(xdp_action::XDP_TX)
 }
 
 #[panic_handler]
