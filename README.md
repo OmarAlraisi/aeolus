ec97cf6d77b1ba98fe2390f595a40ec052d73bce
OmarAlraisi <o.r.raisi@gmail.com>
2024-03-08 22:15:45 +0400

    changes configurations file to take MAC addresses rather than IP

--- README.md
+++ README.md
@@ -4,15 +4,27 @@
 
 This is a proof-of-concept (PoC) implementation of Unimog.
 
+## Description:
+
+**<u>Note:</u>** Since this is a PoC, to make it simpler, Aeolus follows a slightly different approach to transfer packets between servers than Unimog.
+
+### Setup:
+
+For the purpose of testing Aeolus I created two Linux virtual machines(VM), and manually added the same IP address to each of these VMs.
+
+### Transfering packets between servers:
+
+Since all servers share the same IP, I opted to transfer packets between servers by modifying the MAC address of the packet rather than encapsulating the packet with the generic UDP encapsulation method. Therefore, unlike Unimog, Aeolus takes MAC addresses to configure the load balancer rather than direct IP addresses.
+
 ## Usage:
 
 ```
 Usage: aeolus [OPTIONS]
 
 Options:
-  -s, --servers <IP>  Comma separated servers
+  -s, --servers <MAC>             Comma separated servers' MAC addresses
   -p, --ports <PORT>              Comma separated ports [default: 80]
-  -i, --iface <NI>                Netowrk interface of the virtual IP [default: wlp1s0]
+  -i, --iface <NI>                Netowrk interface to attach eBPF app to [default: wlp1s0]
       --logfile <FILE>            Path to log file [default: /var/log/aeolus.log]
       --config <FILE>             Path to Aeolus configuration file
   -h, --help                      Print help
@@ -27,8 +39,8 @@ ports:
   - 80
   - 443
 servers:
-  - 192.168.100.1
-  - 192.168.100.2
-  - 192.168.100.3
+  - 00:00:00:00:00:00
+  - 00:00:00:00:00:01
 logfile: ./aeolus.log
+iface: enp3s0
 ```
--- aeolus/src/config.rs
+++ aeolus/src/config.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use serde::Deserialize;
-use std::{fs, io::BufReader, net::Ipv4Addr, process, str::FromStr};
+use std::{fs, io::BufReader, process, u8};
 
 const DEFAULT_LOG_FILE: &str = "/var/log/aeolus.log";
 const DEFAULT_NETWORK_INTERFACE: &str = "wlp1s0";
@@ -22,17 +22,42 @@ impl ConfigError {
     }
 }
 
+fn mac_str_to_bytes(servers: Vec<String>) -> Result<Vec<[u8; 6]>, ConfigError> {
+    let mac_err = Err(ConfigError::new("Invalid MAC address."));
+    let mut mac_addresses: Vec<[u8; 6]> = Vec::new();
+    for mac_str in servers {
+        let pairs: Vec<&str> = mac_str.split(':').collect();
+
+        if pairs.len() != 6 {
+            return mac_err;
+        }
+
+        let mut bytes = [0; 6];
+        for (idx, pair) in pairs.iter().enumerate() {
+            match u8::from_str_radix(pair, 16) {
+                Ok(pair_byte) => {
+                    bytes[idx] = pair_byte;
+                }
+                Err(_) => return mac_err,
+            };
+        }
+
+        mac_addresses.push(bytes);
+    }
+    Ok(mac_addresses)
+}
+
 #[derive(Parser, Debug)]
 struct Options {
-    /// Comma Seperated servers
-    #[arg(short, long, value_delimiter = ',', value_name = "IP")]
+    /// Comma Seperated servers' MAC addresses
+    #[arg(short, long, value_delimiter = ',', value_name = "MAC")]
     servers: Option<Vec<String>>,
 
     /// Comma Seperated ports [default: 80]
     #[arg(short, long, value_delimiter = ',', value_name = "PORT")]
     ports: Option<Vec<u16>>,
 
-    /// Network interface of the virtual IP
+    /// Network interface to attach eBPF app to
     #[arg(short, long, value_name = "NI", default_value = DEFAULT_NETWORK_INTERFACE)]
     iface: String,
 
@@ -54,12 +79,17 @@ impl Options {
             }
 
             match Options::parse_config_file(config_file) {
-                Ok(file_config) => Ok(Config {
-                    ports: file_config.ports.unwrap_or(vec![80]),
-                    servers: file_config.servers,
-                    log_file: file_config.log_file.unwrap_or(DEFAULT_LOG_FILE.to_string()),
-                    iface: file_config.iface.unwrap_or(DEFAULT_NETWORK_INTERFACE.to_string()),
-                }),
+                Ok(file_config) => {
+                    let servers = mac_str_to_bytes(file_config.servers.clone())?;
+                    Ok(Config {
+                        ports: file_config.ports.unwrap_or(vec![80]),
+                        servers,
+                        log_file: file_config.log_file.unwrap_or(DEFAULT_LOG_FILE.to_string()),
+                        iface: file_config
+                            .iface
+                            .unwrap_or(DEFAULT_NETWORK_INTERFACE.to_string()),
+                    })
+                }
                 Err(e) => Err(e),
             }
         } else {
@@ -69,22 +99,11 @@ impl Options {
                 ));
             }
 
-            let mut servers: Vec<Ipv4Addr> = Vec::new();
-            for server_str in self.servers.clone().unwrap().iter() {
-                match Ipv4Addr::from_str(server_str) {
-                    Ok(server) => {
-                        servers.push(server);
-                    }
-                    Err(e) => return Err(ConfigError::new(e.to_string().as_str())),
-                }
-            }
-
+            let servers = mac_str_to_bytes(self.servers.clone().unwrap())?;
             Ok(Config {
                 ports: self.ports.clone().unwrap_or(vec![80]),
                 servers,
-                log_file: self
-                    .log_file
-                    .clone(),
+                log_file: self.log_file.clone(),
                 iface: self.iface.clone(),
             })
         }
@@ -114,7 +133,7 @@ impl Options {
 #[derive(Deserialize)]
 struct FileConfig {
     ports: Option<Vec<u16>>,
-    servers: Vec<Ipv4Addr>,
+    servers: Vec<String>,
     log_file: Option<String>,
     iface: Option<String>,
 }
@@ -122,7 +141,7 @@ struct FileConfig {
 #[derive(Debug)]
 pub struct Config {
     pub ports: Vec<u16>,
-    pub servers: Vec<Ipv4Addr>,
+    pub servers: Vec<[u8; 6]>,
     pub log_file: String,
     pub iface: String,
 }
